7da6e8b9083824b264c7cf6fe52ada23f92effc8
El panquques <134167396+JoseTaretto@users.noreply.github.com>
2026-06-07 23:41:56 -0300

    Revert "Levels if"

--- public/src/engine/levels/GymScene.js
+++ public/src/engine/levels/GymScene.js
@@ -12,7 +12,6 @@ export class GymScene extends TileLevelScene {
 
   create() {
     super.create();
-    window.__setIfPanel?.(false);
     const els = [
       document.querySelector('[data-dir="func1"]'),
       document.getElementById('queue-func1'),
@@ -22,7 +21,6 @@ export class GymScene extends TileLevelScene {
       if (el) el.style.display = 'none';
     }
     this.events.once('shutdown', () => {
-      window.__setIfPanel?.(true);
       for (const el of els) {
         if (el) el.style.display = '';
       }
